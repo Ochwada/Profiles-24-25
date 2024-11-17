c24f683648345785f63b228d672d7fab9c3ff952
Ochwada <nakhulolinda@yahoo.com>
2024-11-17 16:01:59 +0100

    Header and layout-5

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,10 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  safelist: [
+    'hidden', 'md:flex', 'md:hidden', 'space-x-6', 'mx-auto', 'text-lg', 'font-semibold',
   ],
   theme: {
     extend: {
